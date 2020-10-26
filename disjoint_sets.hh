897913ab0e73acf14f63d9a02c817bc48f845931
hiiragi4000 <xavier13540@gmail.com>
2020-10-26 11:37:21 +0800

    Update disjoint_sets.hh
    struct DisjointSets:
    1. Add noexcept specifiers to all methods
    2. Add method find() const: no path-compression
    3. Add method component_size() const: call const overload of find()
    
    struct DjsCow:
    1. Implementation Changed: maintain size instead of rank
    2. Add noexcept specifiers to find()
    3. Add method component_size()

--- disjoint_sets.hh
+++ disjoint_sets.hh
@@ -6,20 +6,27 @@
 struct DisjointSets{
    DisjointSets() = default;
    explicit DisjointSets(int n): p(n, -1), s(n, 1){}
-   int find(int i){
-      int r = i;
-      while(p[r]!=-1) r = p[r];
+   int find(int i) noexcept{
+      int r = static_cast<DisjointSets const*>(this)->find(i);
       for(int j; i!=r; ) j = p[i], p[i] = r, i = j;
       return r;
    }
-   bool merge(int i, int j){
+   int find(int i) const noexcept{
+      int r = i;
+      while(p[r] != -1) r = p[r];
+      return r;
+   }
+   bool merge(int i, int j) noexcept{
       i = find(i); j = find(j);
       if(i == j) return false;
       if(s[i] < s[j]) std::swap(i, j);
       p[j] = i; s[i] += s[j];
       return true;
    }
-   int component_size(int i){
+   int component_size(int i) noexcept{
+      return s[find(i)];
+   }
+   int component_size(int i) const noexcept{
       return s[find(i)];
    }
 private:
@@ -32,7 +39,7 @@ struct DjsCow: private VlaCow<std::pair<int, int>>{
    using Base::load;
    using Base::save;
    DjsCow() = default;
-   explicit DjsCow(int n): Base(n, {-1, 0}){}
+   explicit DjsCow(int n): Base(n, {-1, 1}){}
    int find(int u) const noexcept{
       int r = u;
       for(int p; (p=std::get<PARENT>((*this)[r]))!=-1; ){
@@ -43,19 +50,20 @@ struct DjsCow: private VlaCow<std::pair<int, int>>{
    bool merge(int u, int v){
       u = find(u); v = find(v);
       if(u == v) return false;
-      int ru = std::get<RANK>((*this)[u]), rv = std::get<RANK>((*this)[v]);
-      if(ru < rv){
+      int su = std::get<SIZE>((*this)[u]), sv = std::get<SIZE>((*this)[v]);
+      if(su < sv){
          std::swap(u, v);
-         std::swap(ru, rv);
+         std::swap(su, sv);
       }
-      write(v, {u, rv});
-      if(ru == rv){
-         write(u, {-1, ru+1});
-      }
+      write(v, {u, sv});
+      write(u, {-1, su+sv});
       return true;
    }
+   int component_size(int u) const noexcept{
+      return std::get<SIZE>((*this)[find(u)]);
+   }
 private:
-   enum {PARENT, RANK};
+   enum {PARENT, SIZE};
 };
 
 #endif
