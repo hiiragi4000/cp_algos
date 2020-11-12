599c93b08756db5b51ccaa33944a3de9a0b1e295
hiiragi4000 <xavier13540@gmail.com>
2020-11-13 05:28:08 +0800

    Polynomial: new functions and bug fixes
    New functions:
    1. log(f(x)) with f(0) = 1, O(n log n)
    2. exp(f(x)) with f(0) = 0, O(n log n)
    3. pow(f(x), m), O(n log n + log m)
    4. sqrt(f(x)), O(n log n)
    
    Bug fixes:
    1. Fix the potential out-of-range bug in operator==() for two polynomials.

--- polynomial.hh
+++ polynomial.hh
@@ -3,6 +3,8 @@
 
 #include"convolution.hh"
 #include<initializer_list>
+#include<memory>
+#include<optional>
 
 #define I64 long long
 
@@ -22,20 +24,17 @@ template<typename NttT> struct PolynomialNtt: private std::vector<int>{
    using Base::clear;
    using Base::get_allocator;
    using Base::reserve;
+   using Base::resize;
    using Base::shrink_to_fit;
    using Base::size;
    PolynomialNtt() = default;
-   PolynomialNtt(int c): PolynomialNtt(Base{c}){}
+   PolynomialNtt(int c): PolynomialNtt(c? Base{c}: Base{}){}
    PolynomialNtt(std::initializer_list<int> coef): PolynomialNtt(Base(coef)){}
    template<typename It> PolynomialNtt(It b, It e): PolynomialNtt(Base(b, e)){}
    explicit PolynomialNtt(Base coef): Base(move(coef)){
       for(size_t i=0; i<size(); ++i){
          data()[i] = ((I64)data()[i]%mod() + mod()) % mod();
       }
-   }
-   PolynomialNtt &resize(size_t n){
-      Base::resize(n);
-      return *this;
    }
    int deg() const noexcept{
       auto it = find_if(crbegin(), crend(), [](int ci){return ci;});
@@ -59,8 +58,32 @@ template<typename NttT> struct PolynomialNtt: private std::vector<int>{
       }
       return (int)res;
    }
-   PolynomialNtt &truncate() noexcept{
-      return resize(deg()+1);
+   PolynomialNtt &operator<<=(int n_terms){
+      insert(cbegin(), n_terms, 0);
+      return *this;
+   }
+   PolynomialNtt operator<<(int n_terms) const{
+      auto res = *this;
+      return res <<= n_terms;
+   }
+   PolynomialNtt &operator>>=(int n_terms){
+      erase(cbegin(), cbegin()+std::min(n_terms, (int)size()));
+      return *this;
+   }
+   PolynomialNtt operator>>(int n_terms) const{
+      if(n_terms < (int)size()){
+         return PolynomialNtt(cbegin()+n_terms, cend());
+      }
+      return {};
+   }
+   PolynomialNtt &truncate(int n_terms) noexcept{
+      if(n_terms < (int)size()){
+         resize(n_terms);
+      }
+      return *this;
+   }
+   PolynomialNtt first_n_terms(int n) const{
+      return {cbegin(), cbegin()+std::min(n, (int)size())};
    }
    PolynomialNtt rev() const{
       int d = deg();
@@ -105,7 +128,7 @@ template<typename NttT> struct PolynomialNtt: private std::vector<int>{
       while((n&-n) != n) n += n&-n;
       resize(n);
       NttT::transform_in_place(false, begin(), n);
-      if(this == &rhs){
+      if(data() == rhs.data()){
          for(int i=0; i<n; ++i){
             data()[i] = (I64)data()[i] * data()[i] % mod();
          }
@@ -116,24 +139,29 @@ template<typename NttT> struct PolynomialNtt: private std::vector<int>{
          }
       }
       NttT::transform_in_place(true, begin(), n);
-      return *this;
+      return truncate(d1+d2+1);
    }
    PolynomialNtt &operator/=(PolynomialNtt const &rhs){
-      auto rf = rev(), rg = rhs.rev();
-      int n = (int)rf.size()-1, d = (int)rg.size()-1;
-      if(n < d){
-         clear(); return *this;
+      int n = deg(), d = rhs.deg();
+      if(d<32 || n-d<32){
+         resize(n+1);
+         return *this = PolynomialNtt(naive_division(std::move(*this), rhs.data(), d).first);
       }
-      *this = rf.resize(n-d+1)*rg.reciprocal(n-d+1).resize(n-d+1);
+      auto rf = rev(), rg = rhs.rev();
+      *this = rf.truncate(n-d+1)*rg.reciprocal(n-d+1);
+      resize(n-d+1);
       reverse(begin(), end());
       return *this;
    }
    PolynomialNtt &operator%=(PolynomialNtt const &rhs){
       int n = deg(), d = rhs.deg();
-      if(n < d) return *this;
+      if(d<32 || n-d<32){
+         resize(n+1);
+         return *this = PolynomialNtt(naive_division(std::move(*this), rhs.data(), d).second);
+      }
       auto q = *this;
       q /= rhs; q *= rhs;
-      resize(d); q.resize(d);
+      truncate(d); q.truncate(d);
       return *this -= q;
    }
    PolynomialNtt derivative() const{
@@ -157,6 +185,7 @@ template<typename NttT> struct PolynomialNtt: private std::vector<int>{
       return res;
    }
    PolynomialNtt reciprocal(int n_terms) const{
+      // assert(coef(0) != 0);
       if(n_terms <= 0) return {};
       int d = deg(), n = n_terms;
       while((n&-n) != n) n += n&-n;
@@ -215,26 +244,102 @@ private:
       }
       return res;
    }
+   static std::pair<std::vector<int>, std::vector<int>> naive_division(vector<int> f, int const *g, int deg_g){
+      int deg_f = (int)f.size()-1;
+      if(deg_f < deg_g){
+         return {{}, move(f)};
+      }
+      vector<int> q(deg_f-deg_g+1);
+      for(int i=deg_f-deg_g; i>=0; --i){
+         I64 qi = f[i+deg_g] * inv_mod(g[deg_g], mod()) % mod();
+         f[i+deg_g] = 0;
+         for(int j=0; j<deg_g; ++j){
+            f[i+j] = (f[i+j] - qi*g[j]%mod() + mod()) % mod();
+         }
+         q[i] = qi;
+      }
+      f.resize(deg_g);
+      return {move(q), move(f)};
+   }
 };
 
+template<typename NttT>
+PolynomialNtt<NttT> operator*(PolynomialNtt<NttT> const &lhs, PolynomialNtt<NttT> const &rhs){
+   PolynomialNtt<NttT> res = lhs;
+   return res *= std::addressof(lhs)==std::addressof(rhs)? res: rhs;
+}
+template<typename NttT>
+PolynomialNtt<NttT> operator*(PolynomialNtt<NttT> &&lhs, PolynomialNtt<NttT> const &rhs){
+   return std::move(lhs*=rhs);
+}
 #define DEF_BIOP(op) template<typename NttT>\
 PolynomialNtt<NttT> operator op(PolynomialNtt<NttT> lhs, PolynomialNtt<NttT> const &rhs){\
    return lhs op##= rhs;\
 }
 DEF_BIOP(+)
 DEF_BIOP(-)
-DEF_BIOP(*)
 DEF_BIOP(/)
 DEF_BIOP(%)
 #undef DEF_BIOP
 template<typename NttT>
 bool operator==(PolynomialNtt<NttT> const &lhs, PolynomialNtt<NttT> const &rhs){
    int d = lhs.deg();
-   return rhs.deg()==d && equal(lhs.cbegin(), lhs.cend(), rhs.cbegin());
+   return rhs.deg()==d && equal(lhs.cbegin(), lhs.cbegin()+(d+1), rhs.cbegin());
 }
 template<typename NttT>
 bool operator!=(PolynomialNtt<NttT> const &lhs, PolynomialNtt<NttT> const &rhs){
    return !(lhs == rhs);
+}
+
+template<typename FpsT>
+FpsT log_fps(FpsT const &f, int n_terms){
+   // assert(f.coef(0) == 1);
+   if(n_terms <= 0) return {};
+   return (f.derivative()*f.reciprocal(n_terms-1)).truncate(n_terms-1).integral();
+}
+
+template<typename FpsT>
+FpsT exp_fps(FpsT const &f, int n_terms){
+   // assert(f.coef(0) == 0);
+   if(n_terms <= 0) return {};
+   if(n_terms == 1) return 1;
+   auto g = exp_fps(f.first_n_terms((n_terms+1)/2), (n_terms+1)/2);
+   return (g*(static_cast<FpsT>(1)-log_fps(g, n_terms)+f)).truncate(n_terms);
+}
+
+template<typename FpsT>
+FpsT pow_fps(FpsT const &f, I64 n, int n_terms){
+   // assert(n >= 0);
+   if(n_terms <= 0) return {};
+   if(n == 0) return 1;
+   int d = f.deg();
+   if(d == -1) return {};
+   int low = 0;
+   while(!f.coef(low)) ++low;
+   if((n_terms-1)/n < low) return {};
+   int m = n_terms-n*low;
+   auto res = f >> low;
+   res /= f.coef(low);
+   res = exp_fps(static_cast<FpsT>(n)*log_fps(res, m), m);
+   res *= pow_mod(f.coef(low), n, FpsT::mod());
+   return res <<= n*low;
+}
+
+template<typename FpsT>
+std::optional<FpsT> sqrt_fps(FpsT const &f, int n_terms){
+   int d = f.deg();
+   if(d == -1) return FpsT{};
+   int low = 0;
+   while(!f.coef(low)) ++low;
+   int c0;
+   if(low % 2 || (c0 = sqrt_modp(f.coef(low), FpsT::mod())) == -1){
+      return {};
+   }
+   FpsT g = f>>low, res = c0;
+   for(int i=1; i<n_terms-low/2; i*=2){
+      res = (res+g.first_n_terms(2*i)*res.reciprocal(2*i))/static_cast<FpsT>(2);
+   }
+   return res << low/2;
 }
 
 #undef I64
