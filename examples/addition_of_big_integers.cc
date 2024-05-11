5c1c979a951831b8bd650b3a8796b1c822215333
hiiragi4000 <xavier13540@gmail.com>
2024-05-11 18:38:48 +0900

    elaborate the APIs of struct BigInt

--- examples/addition_of_big_integers.cc
+++ examples/addition_of_big_integers.cc
@@ -11,12 +11,16 @@ int main(){
    while(T-- > 0){
       string sa, sb;
       cin >> sa >> sb;
-      // BigInt stob(char const *s, size_t *pos = nullptr):
-      // Interprets a BigInt in the string |s|.
-      // First it discards whitespaces (identified by std::isspace) until the first non-whitespace character is found.
+      // BigInt stob(char const *s, size_t *pos=nullptr, int base=10):
+      // Interprets a BigInt from the string |s| in base |base|.
+      // The base must be in {0, 2, 3, ..., 36}; otherwise, an |std::invalid_argument| is thrown.
+      // First the function discards whitespaces (identified by std::isspace) until the first non-whitespace character is found.
       // Then it reads a BigInt consists of the following parts:
       // 1. (optional) plus and minus signs
-      // 2. a (non-empty) sequence of digits
+      // 2. (optional) prefix 0 or 0x/0X, indicating octal or hexadecimal base (applies only when the base is 0)
+      // 3. a (non-empty) sequence of digits
+      // The set of valid digits for base-2 integers is {0, 1}, for base-3 integers is {0, 1, 2}, and so on.
+      // For bases larger than 10, the set of valid digits contains alphabets with the cases ignored.
       // If |pos| is not nullptr, the number of characters consumed is stored in |*pos|.
       // If no conversion can be performed, an |std::invalid_argument| is thrown.
       BigInt a = stob(sa.data()), b = stob(sb.data());
--- examples/division_of_big_integers.cc
+++ examples/division_of_big_integers.cc
@@ -13,10 +13,10 @@ int main(){
       cin >> sa >> sb;
       BigInt a = stob(sa.data()), b = stob(sb.data());
       // DivT<BigInt> div(BigInt const &a, BigInt const &b):
-      // Computes both the quotient `quot` and the remainder `rem` of the division of the numerator `a` by the denominator `b`.
-      // If b = 0, raises SIGFPE.
+      // Computes both the quotient |quot| and the remainder |rem| of the division of the numerator |a| by the denominator |b|.
+      // If |b| = 0, raises SIGFPE.
       // The quotient is the algebraic quotient with any fractional part discarded (truncated towards zero).
-      // The remainder is such that quot*y + rem = x.
+      // The remainder is such that |quot|*|b| + |rem| = |a|.
       // The struct template DivT is defined as follows:
       // template<typename T> struct DivT<T> { T quot, rem; };
       auto [q, r] = div(a, b);
